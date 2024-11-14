ea03578d0a572577e2b85f0bbb08af0d26267e6a
TheDonDev <donaldmwanga33@gmail.com>
2024-11-15 01:11:31 +0300

    Combine elements to generate email

--- Java/src/emailapp/Email.java
+++ Java/src/emailapp/Email.java
@@ -7,9 +7,11 @@ public class Email {
     private String lastName;
     private String password;
     private String department;
+    private String email;
     private int mailboxCapacity;
     private int defaultPasswordLength = 10;
     private String alternateEmail;
+    private String companySuffix = "aeycompany.com";
 
     // Constructor to recieve the first name and last name
     public Email(String firstName, String lastName){
@@ -24,6 +26,10 @@ public class Email {
         // Call a method that returns a random password
         this.password = randomPassword(defaultPasswordLength);
         System.out.println("Your password is: " + this.password);
+
+        // Combine elements to generate email
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix;
+        System.out.println("Your Email is: " + email);
     }
  // Ask for department 
  private String setDepartment(){
